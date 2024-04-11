2e0b445b6a7f6933cfcbfc093b3ad4b64ac438c3
Mohana Misra <iammohanamisra@gmail.com>
2024-04-11 12:43:46 +0530

    Carousel form 1

--- src/components/Speakers/Speakers.jsx
+++ src/components/Speakers/Speakers.jsx
@@ -1,6 +1,57 @@
 import { useState, useEffect } from 'react'
 import './Speakers.css'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import p1 from '../../assets/p1.jpg'
+import p2 from '../../assets/p2.jpg'
+import p3 from '../../assets/p3.jpg'
+import p4 from '../../assets/p4.jpg'
+import p5 from '../../assets/p5.jpg'
+import p6 from '../../assets/p6.jpg'
+
+const slides = [
+    {
+        name: 'Jane',
+        img: p1,
+        desc: '5-time TEDx Speaker'
+    },
+    {
+        name: 'Joe',
+        img: p2,
+        desc: '5-time TEDx Speaker'
+    },
+    {
+        name: 'Max',
+        img: p3,
+        desc: '5-time TEDx Speaker'
+    },
+    {
+        name: 'Karen',
+        img: p4,
+        desc: '5-time TEDx Speaker'
+    },
+    {
+        name: 'Ahmad',
+        img: p5,
+        desc: '5-time TEDx Speaker'
+    },
+    {
+        name: 'Shah',
+        img: p6,
+        desc: '5-time TEDx Speaker'
+    }
+]
 function Speakers() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 5500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+    };
 
     return (
         <div className = 'speakers-container'>
@@ -14,6 +65,19 @@ function Speakers() {
                 </p>
             </div>
             <div className = 'speakers-details'>
+                <Slider {...settings}>
+                {slides.map((d, i) => (
+                    <div className = 'card' key = {i}>
+                        <div>
+                            <img src = {d.img} alt = ''/>
+                        </div>
+                        <div className = 'desc'>
+                            <p>{d.name}</p>
+                            <p>{d.desc}</p>
+                        </div>
+                    </div>
+                ))}
+                </Slider>
             </div>
         </div>
     )
