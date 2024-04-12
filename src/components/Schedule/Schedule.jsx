33e7f1f6225c70b61c025f059560f660db105bdb
Mohana Misra <iammohanamisra@gmail.com>
2024-04-12 11:56:41 +0530

    Designed cards

--- src/components/Schedule/Schedule.jsx
+++ src/components/Schedule/Schedule.jsx
@@ -3,14 +3,27 @@ function Schedule() {
 
     return (
         <div className = 'schedule-container'>
-            <h1 className = 'schedule-heading'>Program Schedule</h1>
+            <h1 className = 'schedule-heading'>The Event Line-Up</h1>
             <div className = 'schedule-cards-container'>
-                <div className = 'card'>
+                <div className = 'schedule-card'>
                     <div className = 'card-left'>
                         <p className = 'card-heading'>Topic 1</p>
-                        <p className = 'card-details'>Guest Speaker – Martin (Tin) Salías</p>
+                        <p className = 'card-details'>Guest Speaker –
+                            <span className = 'schedule-card-name'> Martin (Tin) Salías</span>
+                        </p>
                     </div>
                     <div className = 'card-right'>
+                        <p className = 'time'>08:30 - 10:00</p>
+                    </div>
+                </div>
+                <div className = 'schedule-card'>
+                    <div className = 'card-left'>
+                        <p className = 'card-heading'>Topic 1</p>
+                        <p className='card-details'>Guest Speaker –
+                            <span className='schedule-card-name'> Martin (Tin) Salías</span>
+                        </p>
+                    </div>
+                    <div className='card-right'>
                         <p className = 'time'>08:30 - 10:00</p>
                     </div>
                 </div>
