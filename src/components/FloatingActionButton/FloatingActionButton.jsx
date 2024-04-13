4d362e8bb9b937d8fb382d1db991b3d36954ceac
Mohana Misra <iammohanamisra@gmail.com>
2024-04-13 12:08:31 +0530

    Removed material ui

--- src/components/FloatingActionButton/FloatingActionButton.jsx
+++ src/components/FloatingActionButton/FloatingActionButton.jsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Fab from '@mui/material/Fab';
-import NavigationIcon from '@mui/icons-material/Navigation';
 import './FloatingActionButton.css'
 
 export default function FloatingActionButtons() {
     return (
-        <Box sx={{ '& > :not(style)': { m: 1 } }} className = 'fab-container'>
-            <Fab variant="extended" className = 'fab'>
+        <div className = 'fab-container'>
+            <button className = 'fab'>
                 Buy Ticket
-            </Fab>
-        </Box>
+            </button>
+        </div>
     );
 }
